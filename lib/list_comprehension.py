4b61564f4f82ea58ab43ced70d0bb3a3720069eb
Monica Garcia <monicairenegarcia@gmail.com>
2024-03-21 14:26:00 -0700

    complete comprehensions and generators lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return_evens_list = [element for element in num_list if element%2 == 0]
+    return return_evens_list
 
 def make_exclamation(sentence_list):
-    pass
+    make_exclamation_list = [sentence_list + '!' for sentence_list in sentence_list]
+    return make_exclamation_list
